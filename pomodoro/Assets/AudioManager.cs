f9f10693d95f0989c5df997946313b2494311054
FolderN <100181126+FolderN@users.noreply.github.com>
2023-05-22 00:53:08 +0700

    Sound-Setting
    Глобальная настройка звука (ЭТА НАСТРОЙКА ПРОСТО ИМБА)

--- pomodoro/Assets/AudioManager.cs
+++ pomodoro/Assets/AudioManager.cs
@@ -1,25 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 // Скрипт для управления звуками
 public class AudioManager : MonoBehaviour
 {
+    public Sprite audioOn;
+    public Sprite audioOff;
+    public GameObject audioState;
+
+    public Toggle toggle;
+    public int isOn;
+
+    public AudioSource audio;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (AudioListener.volume == 1)
+        {
+            toggle.isOn = true;
+            audioState.GetComponent<Image>().sprite = audioOn;
+            isOn = 1;
+        }
+        else
+        {
+            toggle.isOn = false;
+            audioState.GetComponent<Image>().sprite = audioOff;
+            isOn = 0;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     // Публичный метод для проигрывания звука
     public void PlayAudio(AudioClip clip)
     {
-        GetComponent<AudioSource> ().PlayOneShot (clip);
+        GetComponent<AudioSource>().PlayOneShot(clip);
+    }
+
+    public void OnOffAudio()
+    {
+        if (isOn == 1)
+        {
+            AudioListener.volume = 0;
+            isOn = 0;
+            audioState.GetComponent<Image>().sprite = audioOff;
+        }
+        else
+        {
+            AudioListener.volume = 1;
+            isOn = 1;
+            audioState.GetComponent<Image>().sprite = audioOn;
+        }
     }
 }
