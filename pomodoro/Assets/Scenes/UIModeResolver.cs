7b83ff4a72296d502d7ff9a6de25b3aa02d2302c
wound3d <128012315+wound3d@users.noreply.github.com>
2023-05-28 18:06:57 +0700

    Update UIModeResolver.cs

--- pomodoro/Assets/Scenes/UIModeResolver.cs
+++ pomodoro/Assets/Scenes/UIModeResolver.cs
@@ -10,12 +10,12 @@ public class UIModeResolver : MonoBehaviour
     public void ResolveMode()
     {
 
-        foreach (var text in GetComponentsInChildren<TMP_Text>())
+        foreach (var text in GetComponentsInChildren<TMP_Text>(true))
         {
             text.color = new Color(255 - text.color[0], 255 - text.color[1], 255 - text.color[2], 1);
         }
 
-        foreach (var image in GetComponentsInChildren<Image>())
+        foreach (var image in GetComponentsInChildren<Image>(true))
         {
             if (image.color[3] != 0) image.color = new Color(1 - image.color[0], 1 - image.color[1], 1 - image.color[2], 1);
         }
